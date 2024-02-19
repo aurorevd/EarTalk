fc6c079b19f17b9b16cdbe1587f3d10b6b29343c
aurorevd <aurorevandamme@gmail.com>
2024-02-19 14:42:51 +0100

    add colors

--- src/components/TopicsContainer.jsx
+++ src/components/TopicsContainer.jsx
@@ -2,12 +2,13 @@ import styled from "styled-components";
 
 const StyledTopicsContainer = styled.div`
   width: 50%;
-  height:25vh;
+  height: 25vh;
   display: flex;
   flex-wrap: wrap;
-row-gap:1vw;
+  row-gap: 1vw;
   justify-content: space-between;
 `;
+
 const TopicButton = styled.button`
   width: 7vw;
   height: 8vh;
@@ -18,22 +19,30 @@ const TopicButton = styled.button`
   color: white;
   font-size: 16px;
   font-weight: black;
-  background-color: var(--purple);
+  background-color: ${props => props.color};
   border: solid 1px var(--grey);
   border-radius: var(--radius);
 `;
 
-
 function TopicsContainer() {
+
+  const topics = [
+    { name: "Science", color: "var(--blue)" },
+    { name: "Culture", color: "var(--purple)" },
+    { name: "Sport", color: "var(--red)" },
+    { name: "Society", color: "var(--pink)" },
+    { name: "History", color: "var(--yellow)" },
+    { name: "Health", color: "var(--green)" }
+  ];
+
   return (
-        <StyledTopicsContainer>
-          <TopicButton>Science</TopicButton>
-          <TopicButton>Culture</TopicButton>
-          <TopicButton>Sport</TopicButton>
-          <TopicButton>Society</TopicButton>
-          <TopicButton>History</TopicButton>
-          <TopicButton>Health</TopicButton>
-        </StyledTopicsContainer>
+    <StyledTopicsContainer>
+      {topics.map((topic, index) => (
+        <TopicButton key={index} color={topic.color}>
+          {topic.name}
+        </TopicButton>
+      ))}
+    </StyledTopicsContainer>
   );
 }
 
